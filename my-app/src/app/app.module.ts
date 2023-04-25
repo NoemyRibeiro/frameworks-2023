a2ce2cc608290218b991c473fcd379d8db504fb8
NoemyRibeiro <127446089+NoemyRibeiro@users.noreply.github.com>
2023-04-25 15:54:36 -0400

    Add files via upload

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
